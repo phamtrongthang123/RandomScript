ce9c3dfacf72ecc1fb3f5ba044b7dc7c164c17ff
Trong-Thang Pham <phamtrongthang123@gmail.com>
2021-01-03 17:51:46 +0700

    update try catch when downloading

--- crawl_mangadex/utils.py
+++ crawl_mangadex/utils.py
@@ -21,30 +21,37 @@ def get_and_filtering_chapter_list(cid, lang_filter='English', start_chapter=1, 
     return filtered_list_chapter
 
 def start_download(root, filtered_list_chapter):
+    missing_list = []
     for chaptid,v in tqdm(filtered_list_chapter, total=len(filtered_list_chapter)):
-        title = v['title']
-        chapt_data = requests.get(f'https://mangadex.org/api/?id={chaptid}&server=null&saver=0&type=chapter').json()
-        chapter_order = chapt_data['chapter']
-        hash_code = chapt_data['hash']
-        list_pages = chapt_data['page_array']
-        server = chapt_data['server'] # example: https://mangadex.org/data/
-        group_name = chapt_data['group_name']
         try:
-            os.mkdir(f'{root}/{group_name}_{chapter_order}_{chaptid}')
-        except:
-            pass
+            title = v['title'].replace('?','')
+            chapt_data = requests.get(f'https://mangadex.org/api/?id={chaptid}&server=null&saver=0&type=chapter').json()
+            chapter_order = chapt_data['chapter']
+            hash_code = chapt_data['hash']
+            list_pages = chapt_data['page_array']
+            server = chapt_data['server'] # example: https://mangadex.org/data/
+            group_name = chapt_data['group_name'].replace('?','')
+            try:
+                os.mkdir(f'{root}/{group_name}_{chapter_order}_{chaptid}')
+            except:
+                pass
 
-        im_list = []
-        for page_name in list_pages:
-            # no / between server and hash code
-            img_raw_url = f'{server}{hash_code}/{page_name}'
-            r = requests.get(img_raw_url, allow_redirects=True)
-            filename = img_raw_url.split('/')[-1]
-            save_path = f'{root}/{group_name}_{chapter_order}_{chaptid}/{filename}'
-            open(save_path, 'wb').write(r.content)
-            img = Image.open(BytesIO(r.content))
-            img.load()
-            img = img.convert('RGB')
-            im_list.append(img)
-        pdf_fn = f'{root}/Ch. {chapter_order}-{title}-{group_name}.pdf'
-        im_list[0].save(pdf_fn, "PDF" ,resolution=100.0, save_all=True, append_images=im_list[1:])
+            im_list = []
+            for page_name in list_pages:
+                # no / between server and hash code
+                img_raw_url = f'{server}{hash_code}/{page_name}'
+                r = requests.get(img_raw_url, allow_redirects=True)
+                filename = img_raw_url.split('/')[-1]
+                save_path = f'{root}/{group_name}_{chapter_order}_{chaptid}/{filename}'
+                open(save_path, 'wb').write(r.content)
+                img = Image.open(BytesIO(r.content))
+                img.load()
+                img = img.convert('RGB')
+                im_list.append(img)
+            pdf_fn = f'{root}/Ch. {chapter_order}-{title}-{group_name}.pdf'
+            im_list[0].save(pdf_fn, "PDF" ,resolution=100.0, save_all=True, append_images=im_list[1:])
+
+        except:
+            missing_list.append(float(v['chapter']))
+    if len(missing_list) >0:
+        print('missing those ', missing_list)
